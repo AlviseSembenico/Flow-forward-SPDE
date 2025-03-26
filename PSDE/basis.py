fix basis computation

PSDE/basis.py
@@ -1,6 +1,7 @@
 from typing import *
 
 import numpy as np
+import torch
 
 
 @np.vectorize
@@ -15,9 +16,17 @@ def e_1(ξ: float):
     return 1
 
 
+def e_1_torch(ξ: torch.Tensor) -> torch.Tensor:
+    return torch.ones_like(ξ)
+
+
 @np.vectorize
 def e_2(ξ: float):
     return np.exp(-ξ) - 1
+
+
+def e_2_torch(ξ: torch.Tensor) -> torch.Tensor:
+    return torch.exp(-ξ) - 1
 
 
 @np.vectorize
@@ -25,9 +34,17 @@ def e_3(ξ: float):
     return ξ * np.exp(-ξ)
 
 
+def e_3_torch(ξ: torch.Tensor) -> torch.Tensor:
+    return ξ * torch.exp(-ξ)
+
+
 @np.vectorize
 def e_4(ξ: float):
-    return 1 / 2 * (ξ * 2 - 2 * ξ) * np.exp(-ξ)
+    return 1 / 2 * (ξ**2 - 2 * ξ) * np.exp(-ξ)
+
+
+def e_4_torch(ξ: torch.Tensor) -> torch.Tensor:
+    return 1 / 2 * (ξ**2 - 2 * ξ) * torch.exp(-ξ)
 
 
 @np.vectorize
@@ -37,11 +54,23 @@ def e_5(ξ: float):
     return num / den * np.exp(-ξ)
 
 
+def e_5_torch(ξ: torch.Tensor) -> torch.Tensor:
+    num = ξ**3 - 36 * ξ * 2 - 6 * ξ
+    den = 42 * torch.sqrt(torch.tensor(5.0))
+    return num / den * torch.exp(-ξ)
+
+
 @np.vectorize
 def e_6(ξ: float):
     num = ξ**4 - 1440 * ξ**3 - 192 * ξ**2 + 24 * ξ
     den = 24 * np.sqrt(806115)
     return num / den * np.exp(-ξ)
+
+
+def e_6_torch(ξ: torch.Tensor) -> torch.Tensor:
+    num = ξ**4 - 1440 * ξ**3 - 192 * ξ**2 + 24 * ξ
+    den = 24 * torch.sqrt(torch.tensor(806115.0))
+    return num / den * torch.exp(-ξ)
 
 
 @np.vectorize
@@ -51,7 +80,23 @@ def e_7(ξ: float):
     return num / den * np.exp(-ξ)
 
 
+def e_7_torch(ξ: torch.Tensor) -> torch.Tensor:
+    num = ξ**5 - 100800 * ξ**4 - 10800 * ξ**3 - 1200 * ξ**2 - 120 * ξ
+    den = 1560 * torch.sqrt(torch.tensor(49407661.0))
+    return num / den * torch.exp(-ξ)
+
+
 e_basis = [e_1, e_2, e_3, e_4, e_5, e_6, e_7]
+
+e_basis_torch = [
+    e_1_torch,
+    e_2_torch,
+    e_3_torch,
+    e_4_torch,
+    e_5_torch,
+    e_6_torch,
+    e_7_torch,
+]
 
 
 def K_projection(x: np.ndarray, τ: float, k: int) -> np.ndarray:
@@ -71,3 +116,7 @@ def K_projection_standard(x: np.ndarray, T: float, k: int, M: int = 100) -> np.n
 
 def span(τ: np.ndarray) -> np.ndarray:
     return np.array([e_basis[i](τ) for i in range(7)])
+
+
+def span_torch(τ: torch.Tensor) -> torch.Tensor:
+    return torch.stack([e_basis_torch[i](τ) for i in range(7)])

PSDE/data_gpu.py
@@ -4,7 +4,7 @@ import click
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from basis import span
+from basis import span_torch
 from icecream import ic
 
 device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -26,12 +26,10 @@ def generate(
     x0 = torch.empty(size, 7, device=device).uniform_(-1 / 2, 1 / 2)
 
     # Convert partition to tensor and move to device
-    partition_tensor = torch.tensor(partition, dtype=torch.float32, device=device)
+    partition = torch.tensor(partition, dtype=torch.float32, device=device)
 
     # Add the first value from x0
-    space = torch.tensor(
-        span(partition.astype(float) + t), dtype=torch.float32, device=device
-    )
+    space = span_torch(partition + t)
     xt = torch.matmul(x0, space)
 
     dt = t / N
@@ -44,7 +42,7 @@ def generate(
 
     for i in range(N):
         s = dt * i
-        p = partition_tensor + (t - s)
+        p = span_torch(partition + (t - s))
         values = p**2 * dt
 
         lebesgue += values
@@ -93,9 +91,7 @@ def generate_MC(
     partition_tensor = torch.tensor(partition, dtype=torch.float32, device=device)
 
     # Add the first value from x0
-    space = torch.tensor(
-        span(partition.astype(float) + t), dtype=torch.float32, device=device
-    )
+    space = span_torch(partition + t)
     xt = torch.matmul(x0, space)  # shape: (size, partition.shape[0])
 
     dt = t / N
@@ -109,7 +105,7 @@ def generate_MC(
 
     for i in range(N):
         s = dt * i
-        p = partition_tensor + (t - s)
+        p = span_torch(partition + (t - s))
         values = p**2 * dt
         lebesgue += values
 